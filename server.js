14afebb026f0a68769f5d8c9dccbc18fcc0b8e4d
JoanSants <joansants07@gmail.com>
2018-10-11 16:55:37 -0300

    Deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require ('http');
 const app = require('./app.js');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(port, () => {
     console.log(`Up on port ${port}.`);
